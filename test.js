ef5b9f1f1d04556424ed4e37b51e7b84f681cec4
rommel dalisay <rommelmdalisay@gmail.com>
2025-05-10 12:07:06 +0800

    js

--- test.js
+++ test.js
@@ -58,4 +58,103 @@
 // console.log("Foo" && "Bar"); //First operand is truthy, return the second operand
 // console.log(null || "Food"); //First operand is falsy - return second operand
 // console.log("Foo" || "Bar"); //First operand is truthy, return it
-console.log({} || false)
+// console.log({} || false)
+
+// function greeting(name) {
+//     if (name === undefined) {
+//         name = "joEY"
+//     }
+    
+//     name = name && "John";
+//     console.log(name);
+//     console.log("Hello " + name);
+// }
+// greeting("Johnson"); // alerts "Hi Johnson";
+// greeting();
+
+// function isAllowedToDrive(age) {
+//     if (age > 21) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// console.log(isAllowedToDrive(22));
+// sayDay(1) //Sunday
+// sayDay(3)
+// function sayDay(day) {
+//     switch (day) {
+//         case 1: console.log("Sunday");
+//             break;
+//         case 2: console.log("Monday");
+//             break;
+//         default:
+//             console.log("We live in a binary world. Go to Pluto");
+//     }
+// }
+// c = add(1, 2);
+// function add(a, b) {
+//     return a + b;
+// }
+
+// console.log(c);
+
+// c = add(1, 2);
+// const add = function (a, b) {
+//     return a + b;
+// }
+
+
+// const add = (a, b) => a + b
+// c = add(1, 2);
+// console.log(c);
+
+const obj = {
+    "full name": "mike",
+    address: ['address1', "address2"],
+    phone: {
+        phone1: "9999",
+        phone2: "8888",
+        },
+    zip: function zip(){
+        return '1456'
+    }
+}
+
+// console.log(obj.address)
+console.log(obj.zip(), obj["full name"])
+
+var colors = ['red', 'green', 'blue', 'white'];
+colors.forEach(color => console.log(color)
+);
+
+var founders = new Map();
+founders.set("facebook", "mark");
+founders.set("google", "larry");
+console.log(founders.size); // 2
+console.log(founders.get("twitter")); // undefined
+console.log(founders.has("google")); // false
+for (var [key, value] of founders) {
+    console.log(key + " founded by " + value);
+
+    var author = {
+    firstname: "Douglas",
+    lastname: "Crockford",
+    book: {
+        title: "JavaScript- The Good Parts",
+        pages: "172"
+    }
+};
+console.log(author['firstname']); //Douglas
+console.log(author.lastname); //Crockford
+console.log(author.book.title);
+
+var mySet = new Set();
+mySet.add(1);
+mySet.add("Howdy");
+mySet.add("foo");
+console.log(mySet.has(1)); // true
+mySet.delete("foo");
+console.log(mySet.size); // 2
+for (let item of mySet) console.log(item);
+}
